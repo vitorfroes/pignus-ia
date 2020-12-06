dc483c4c53c948d0de2aa449c388033e82539456
Caio Arrabal <caio.a.1998@gmail.com>
2020-12-06 20:12:31 -0300

    Criado servidor a partir do yolo

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Solução IA do sistema Pignus para detectar armas de fogo de pequeno porte.
 
 * [Tutorial de utilização](https://github.com/vitorfroes/pignus-ia/tutorial.ipynb)&nbsp;
 * [Tutorial Yolov5](https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data)&nbsp;
+
+## Executando o Servidor
+Instale as dependências necessárias e execute o servidor (por padrão, ele é executado na porta 3001):
+```bash
+pip install -r requirements.txt
+python server.py
+```
+
+## Observação
+O servidor da IA deve estar na mesma máquina que o servidor mestre.
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 from pathlib import Path
 
@@ -71,7 +72,7 @@ def detect(source, save_img=False):
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img:  # Add bbox to image
+                    if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
@@ -90,6 +91,28 @@ def detect(source, save_img=False):
             }
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    opt = parser.parse_args()
+    #print(opt)
+
+    view_img = opt.view_img
+    
     # Constants
     weights = 'exported_models/custom_yolov5l/weights/best.pt'
     imgsz = 416 # Tamanho de imagem em que a IA foi treinada
